741f553c4fee52c218effb81589d78884f3c2551
PG_Axis <axis.sixtikcz@gmail.com>
2025-04-28 22:38:31 +0200

    Add files via upload

---
+++ MusicPlayer/Favourites.xaml.cs
@@ -0,0 +1,9 @@
+namespace MusicPlayer;
+
+public partial class Favourites : ContentPage
+{
+	public Favourites()
+	{
+		InitializeComponent();
+	}
+}
--- MusicPlayer/MainPage.xaml.cs
+++ MusicPlayer/MainPage.xaml.cs
@@ -1,25 +1,46 @@
-﻿namespace MusicPlayer
+﻿using System.Collections.ObjectModel;
+
+namespace MusicPlayer
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<TabItem> TabItems { get; set; } = new();
+        public TabItem SelectedTab { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+
+            TabItems.Add(new TabItem("Page 1", new Favourites()));
+            /*TabItems.Add(new TabItem("Page 2", new Page2()));
+            TabItems.Add(new TabItem("Page 3", new Page3()));
+            TabItems.Add(new TabItem("Page 4", new Page4()));
+            TabItems.Add(new TabItem("Page 5", new Page5()));
+            TabItems.Add(new TabItem("Page 6", new Page6()));*/
+
+            SelectedTab = TabItems.First();
+            
+
+            TabBar.SelectionChanged += (s, e) => UpdateContent();
+            UpdateContent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void UpdateContent()
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            TabContentView.Content = SelectedTab?.Content.Content;
         }
     }
 
+    public class TabItem
+    {
+        public string Title { get; set; }
+        public ContentPage Content { get; set; }
+
+        public TabItem(string title, ContentPage content)
+        {
+            Title = title;
+            Content = content;
+        }
+    }
 }
