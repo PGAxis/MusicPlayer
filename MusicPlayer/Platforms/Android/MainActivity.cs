56f83aad82ef15d520b702fbd5445bd3be2650b6
PG_Axis <axis.sixtikcz@gmail.com>
2025-05-02 18:50:40 +0200

    Add files via upload

--- MusicPlayer/Platforms/Android/MainActivity.cs
+++ MusicPlayer/Platforms/Android/MainActivity.cs
@@ -7,5 +7,15 @@ namespace MusicPlayer
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Change the status bar color to transparent
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+            }
+        }
     }
 }
