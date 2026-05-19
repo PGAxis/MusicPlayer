eef172a027ec91e729d2c28553e6a8bc1c727f02
Jan Sixta <209504615+PGAxis@users.noreply.github.com>
2026-05-19 19:48:48 +0200

    Visual Update Bug fixes: - Deleted songs now get removed from queue - The expandable player bar now cannot be swiped down when on the Queue screen - The title tab row now properly works with taps
    Additions:
    - 4 new themes
    - "Now playing" song has the equalizer bars even outside of Queue screen
    - "Now playing" bar (expandable player in its shrunken state) transitions from one background color to another

--- app/src/main/java/dev/pgaxis/musicaxs/ui/theme/Color.kt
+++ app/src/main/java/dev/pgaxis/musicaxs/ui/theme/Color.kt
@@ -3,7 +3,6 @@ package dev.pgaxis.musicaxs.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Grays
-val Gray100 = Color(0xFFE1E1E1)
 val Gray200 = Color(0xFFC8C8C8)
 val Gray300 = Color(0xFFACACAC)
 val Gray400 = Color(0xFF919191)
@@ -21,18 +20,13 @@ val CardCyanDark = Color(0xFF1A2332)
 
 //Blues / Cyans / Aquas
 val CyanPrimary = Color(0xFF00BCD4)
-val CyanLight = Color(0xFF4DD0E1)
-val AquaAccent = Color(0xFF00E5FF)
 val BlueSecondary = Color(0xFF1565C0)
 val BlueTertiary = Color(0xFF0288D1)
-val BlueLight = Color(0xFF29B6F6)
-val MidnightBlue = Color(0xFF0A1628)
 
 // Text
 val TextWhitePrimary = Color(0xFFE8E8E8)
 val TextDarkPrimary = Color(0xFF1A1A1A)
 val TextCyanSecondary = Color(0xFFB0B0B0)
-val TextOnButton = Color(0xFFB2EBF2)
 
 // Dividers / Borders
 val DividerCyanColor = Color(0xFF00ACC1)
